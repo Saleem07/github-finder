2bd7ae8533ab3de3204a2a5c9369c2de42578fde
Saleem07 <mohammadsaleem07@outlook.com>
2020-05-09 08:15:06 +0530

    will display list of latest 4 repos of the user.

---
+++ app.js
@@ -0,0 +1,21 @@
+const github = new Github();
+const ui = new UI();
+const searchUser = document.getElementById("searchUser");
+
+searchUser.addEventListener("keyup", (e) => {
+  const userText = e.target.value;
+  if (userText !== "") {
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        //show alert
+        ui.showAlert("Not Found!", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    //Clear profile
+    ui.clearProfile();
+  }
+});
